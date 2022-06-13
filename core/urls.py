8de01a811bae95b9312f91f9da22b28eceb1d6ac
fateme-zka <fatemezahrakalantarii@gmail.com>
2022-06-13 21:58:05 +0430

    SignUp functionality added

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from .views import *
 
+
+app_name = 'core'
+
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('signup/', signup, name='signup'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.views.decorators.csrf import csrf_exempt
+
+from .forms import SignUpForm
 
 
 def frontpage(request):
-
     context = {}
     return render(request, 'core/base.html', context)
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('core:frontpage')
+    else:
+        form = SignUpForm()
+    return render(request, 'core/signup.html', {'form': form})
